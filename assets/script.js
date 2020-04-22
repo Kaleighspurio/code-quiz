9fcc2140fbe5b28f4e54bf1e80204d5d9961a1e5
Kaleigh Spurio <kaleighastell@gmail.com>
2020-04-22 19:43:14 -0400

    Changed my approach to using an array of objects for the questions

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,61 @@
 var startButton = document.querySelector(".start-button");
 var instructions = document.querySelector(".instructions-panel");
 var questionOne = document.querySelector(".question-one");
-var questionTwo = document.querySelector(".question-two");
-var questionThree = document.querySelector(".question-three");
 var timer = document.querySelector(".time-remaining");
-var correct =  document.querySelector(".correct-answer");
-var wrongOne = document.querySelector(".wrong1");
-var wrongTwo = document.querySelector(".wrong2");
-var wrongThree = document.querySelector(".wrong3");
 var answerFeedback = document.querySelector(".right-or-wrong");
-var correctTwo =  document.querySelector(".correct-answer-two");
+
+
+
+var questionArray = [
+    {
+        question: "How many staircases does Hogwarts School of Witchcraft and Wizardry have?",
+        a: "203", 
+        b: "142", 
+        c: "156",
+        d: "78",
+        correct: 1
+    },
+    {
+        question: "Where is the slytherin common room located?",
+        a: "The dungeons", 
+        b: "under the stairs in the broomcloset", 
+        c: "Next to the kitchens", 
+        d: "Behind the portrait of the fat lady",
+        correct : 0
+    },
+    {
+        question: "Who is the Gryffindor house ghost?",
+        a: "the Bloody Baron", 
+        b: "Peeves", 
+        c: "The grey lady", 
+        d: "Nearly Headless Nick",
+        correct: 3
+    },
+    {
+        question: "What does the Sorcerer's Stone do?",
+        a: "Turns the person holding it into an animal", 
+        b: "Produces the elixer of life", 
+        c: "Makes the holder invisible", 
+        d: "Shows the holder what he most desires",
+        correct: 1
+    },
+    {
+        question: "Who is Fluffy?",
+        a: "Fluffy is the pet name Mrs. Dursley has for her son, Dudley", 
+        b: "Hagrid's Dragon", 
+        c: "Ron's rat", 
+        d: "A three headed dog",
+        correct: 3
+    },
+    {
+        question: "How did Moaning Myrtle die?",
+        a: "A mountain troll", 
+        b: "The Whomping Willow", 
+        c: "The Basilisk", 
+        d: "The Killing Curse",
+        correct: 2
+    }
+];
 
 
 startButton.addEventListener("click", function(){
@@ -19,12 +65,12 @@ startButton.addEventListener("click", function(){
     var secondsLeft = 60
     function setTime() {
         var timeInterval = setInterval(function(){
-            --secondsLeft;
+            secondsLeft--;
             timer.textContent = secondsLeft;
 
             if (secondsLeft === 0) {
                 clearInterval(timeInterval);
-                alert("game over");
+                // alert("game over");
                 // make a function that sends a message...
             }
         }, 1000);
@@ -34,7 +80,18 @@ startButton.addEventListener("click", function(){
     // instructions disappear and the first question displayed
     instructions.setAttribute("style", "display: none;");
     questionOne.setAttribute("style", "display: content;");
+    
+    
+    questionArray.forEach(function(question){
+        $(".questions-display").text(question.question);
+        $("#button1").html(question.a);
+        $(".option2").html(question.b);
+        $(".option3").html(question.c);
+        $(".option4").html(question.d);
+    });
 });
+
+
 
 
 // Function for incorrect guesses to deduct seconds and display "Wrong!"
@@ -48,22 +105,20 @@ function wrongAnswer() {
     answerFeedback.textContent = "Wrong!";
 }
 
-// PUT THE QUESTIONS IN AN ARRAY AND SET A LOOP!??? foreach?
-var questionArray = [questionOne, questionTwo, questionThree];
 
-wrongOne.addEventListener("click", function(){
-    wrongAnswer();
-});
-wrongTwo.addEventListener("click", function(){
-    wrongAnswer();
-});
-wrongThree.addEventListener("click", function(){
-    wrongAnswer();
-});
-correct.addEventListener("click", function(){
-    questionOne.setAttribute("style", "display: none;");
-    questionTwo.setAttribute("style", "display: content;");
-});
+// wrongOne.addEventListener("click", function(){
+//     wrongAnswer();
+// });
+// wrongTwo.addEventListener("click", function(){
+//     wrongAnswer();
+// });
+// wrongThree.addEventListener("click", function(){
+//     wrongAnswer();
+// });
+// correct.addEventListener("click", function(){
+//     questionOne.setAttribute("style", "display: none;");
+//     questionTwo.setAttribute("style", "display: content;");
+// });
 
 
 // This isn't working...
