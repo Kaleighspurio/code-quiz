ca2f85f980f6c7cd67b0bf0e76c6c7ab6aac7e70
Kaleigh Spurio <kaleighastell@gmail.com>
2020-04-25 16:12:23 -0400

    Got rid of for and foreach loops, added functions to display questions and check answers

--- assets/script.js
+++ assets/script.js
@@ -4,57 +4,29 @@ var startButton = document.querySelector(".start-button");
 var instructions = document.querySelector(".instructions-panel");
 var questionOne = document.querySelector(".question-one");
 var timer = document.querySelector(".time-remaining");
-var answerFeedback = document.querySelector(".right-or-wrong");
+var i = 0;
 
-
+// Have an array of all questions, choices and the correct answer
 
 var questionArray = [
     {
-        question: "How many staircases does Hogwarts School of Witchcraft and Wizardry have?",
-        a: "203", 
-        b: "142", 
-        c: "156",
-        d: "78",
-        correct: 1
-    },
-    {
-        question: "Where is the slytherin common room located?",
-        a: "The dungeons", 
-        b: "under the stairs in the broomcloset", 
-        c: "Next to the kitchens", 
-        d: "Behind the portrait of the fat lady",
-        correct : 0
-    },
-    {
         question: "Who is the Gryffindor house ghost?",
-        a: "the Bloody Baron", 
-        b: "Peeves", 
-        c: "The grey lady", 
-        d: "Nearly Headless Nick",
+        choices: ["the Bloody Baron", "Peeves", "The grey lady", "Nearly Headless Nick"],
         correct: 3
     },
     {
         question: "What does the Sorcerer's Stone do?",
-        a: "Turns the person holding it into an animal", 
-        b: "Produces the elixer of life", 
-        c: "Makes the holder invisible", 
-        d: "Shows the holder what he most desires",
+        choices: ["Turns the person holding it into an animal", "Produces the elixer of life", "Makes the holder invisible", "Shows the holder what he most desires"],
         correct: 1
     },
     {
         question: "Who is Fluffy?",
-        a: "Fluffy is the pet name Mrs. Dursley has for her son, Dudley", 
-        b: "Hagrid's Dragon", 
-        c: "Ron's rat", 
-        d: "A three headed dog",
+        choices: ["Fluffy is the pet name Mrs. Dursley has for her son, Dudley", "Hagrid's Dragon", "Ron's rat", "A three headed dog"],
         correct: 3
     },
     {
         question: "How did Moaning Myrtle die?",
-        a: "A mountain troll", 
-        b: "The Whomping Willow", 
-        c: "The Basilisk", 
-        d: "The Killing Curse",
+        choices: ["A mountain troll", "The Whomping Willow", "The Basilisk", "The Killing Curse"],
         correct: 2
     }
 ];
@@ -63,56 +35,72 @@ var questionArray = [
 // Function for incorrect guesses to deduct seconds and display "Wrong!"
 function wrongAnswer() {
     // subtract time from the timer
-
     // This subtract 20 doesn't work!!!! FIX MEEEEE
-            // var secondsLeft = document.querySelector(".time-remaining");
+    // var secondsLeft = document.querySelector(".time-remaining");
     // secondsLeft - 20;
     // timer.textContent = secondsLeft;
     answerFeedback.textContent = "Wrong!";
 }
+var secondsLeft = 60
+function setTime() {
+    var timeInterval = setInterval(function () {
+        secondsLeft--;
+        timer.textContent = secondsLeft;
 
+        if (secondsLeft === 0) {
+            clearInterval(timeInterval);
+            // alert("game over");
+            // store the time left on the timer
+            // make it take you to the highscores page- get the score from local storage
+        }
+    }, 1000);
+}
 
-startButton.addEventListener("click", function(){
-    // start the timer
-    var secondsLeft = 60
-    function setTime() {
-        var timeInterval = setInterval(function(){
-            secondsLeft--;
-            timer.textContent = secondsLeft;
+// When the start button is clicked, start the timer
+startButton.addEventListener("click", function () {
 
-            if (secondsLeft === 0) {
-                clearInterval(timeInterval);
-                // alert("game over");
-                // make a function that sends a message...
-            }
-        }, 1000);
-    }
     setTime();
-
     // instructions disappear and the first question displayed
     instructions.setAttribute("style", "display: none;");
     questionOne.setAttribute("style", "display: content;");
-    
-    
-    questionArray.forEach(function(question){
-        $(".questions-display").text(question.question);
-        $(".option1").text(question.a);
-        $(".option2").text(question.b);
-        $(".option3").text(question.c);
-        $(".option4").text(question.d);
-        var correctAnswer = question.correct;
-        console.log(correctAnswer);
-        $("buttons").on("click", function(){
-            alert("button clicked")
-        })
-        $(".buttons").on("click", function(){
-//             if () {
-// // if what they clicked = correctAnswer, then continue to the next question...
-//             } else {
-//                 wrongAnswer();
-//             }
-        });
-       
-    });
+    displayQuestions();
+
 });
+
+function displayQuestions() {
+    $(".questions-display").text(questionArray[i].question);
+    $("#button0").text(questionArray[i].choices[0])
+    $("#button1").text(questionArray[i].choices[1])
+    $("#button2").text(questionArray[i].choices[2])
+    $("#button3").text(questionArray[i].choices[3])
+    console.log(questionArray[i].choices);
+    i++;
+    if (i === 5) {
+        endGame();
+    }
+    
+}
+
+function endGame() {
+    $(".question-box").hide();
+}
+
+function checkAnswer(){
+    $(".right-or-wrong").show();
+    var buttonClicked = event.target;
+    var chosenAnswerIndex = buttonClicked.getAttribute("data-index");
+    console.log(chosenAnswerIndex);
+    console.log("correct answer is " + questionArray[i-1].choices);
+    if (chosenAnswerIndex == questionArray[i-1].correct){
+        $(".right-or-wrong").text("Correct!");
+    } else {
+        $(".right-or-wrong").text("Wrong!");
+    }
+    setTimeout(function(){
+        $(".right-or-wrong").hide();
+    }, 1000);
+    displayQuestions();
+}
+$(".buttons").on("click", checkAnswer);
+
 
