011b52da6bd6b9d89ea95ba095b27de954f8395b
Kaleigh Spurio <kaleighastell@gmail.com>
2020-07-28 09:39:47 -0400

    switched highscorescript to es6

--- assets/highscorescript.js
+++ assets/highscorescript.js
@@ -1,19 +1,16 @@
-
-displayHighScores();
-
 // the displayHighScores function gets the scores from local storage and JSON.parse's them.  Then it stores the high scores onto a table
-function displayHighScores(){
-    var showHighScore = JSON.parse(localStorage.getItem("scores"));
+const displayHighScores = () => {
+    const showHighScore = JSON.parse(localStorage.getItem("scores"));
     console.log(showHighScore);
-    var tableBodyElement = $("#score-body");
-    var tableRowElement;
+    const tableBodyElement = $("#score-body");
+    let tableRowElement;
     if (showHighScore === null){
         $("#score-body").text(" ")
     } else {
         showHighScore.forEach(function(score){
             tableRowElement = $("<tr>");
-            var nameTd = $("<td>").text(score.name);
-            var scoreTd = $("<td>").text(score.score);
+            const nameTd = $("<td>").text(score.name);
+            const scoreTd = $("<td>").text(score.score);
             tableRowElement.append(nameTd);
             tableRowElement.append(scoreTd);
             tableBodyElement.append(tableRowElement);
@@ -21,8 +18,10 @@ function displayHighScores(){
     }
 }
 
+displayHighScores();
+
 // When the clear highscores button is clicked, the local storage is cleared and the displayHighScores function is run, to display the empty scores table.
-$(".clear-scores").on("click", function(){
+$(".clear-scores").on("click", () => {
     localStorage.clear();
     displayHighScores();
 });
